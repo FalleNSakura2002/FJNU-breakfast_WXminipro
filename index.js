ab9a6135b0be51ee25333c329f54c14709650f98
FalleNSakura2002 <531982114@qq.com>
2023-01-08 23:36:50 +0800

    这是用于测试数据库的demo3

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.get("/api/wx_openid", async (req, res) => {
 
 //用于测试插入数据的方法
 app.get("/api/adduser", async (req, res) => {
-  const User_mem1 = await Userdata.create();
+  await Userdata.create();
   res.send("用户添加成功");
 });
 
